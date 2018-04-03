d9f17c8a8eb56fd24f297b7326f1e0d344d6cfa3
mevey <muthoni90@gmail.com>
2018-04-03 12:06:57 -0700

    new UI

--- app.py
+++ app.py
@@ -7,16 +7,34 @@ from models import Committee
 from database import db_session
 
 app = Flask(__name__)
-app.secret_key = os.environ['APP_SECRET_KEY']
+#app.secret_key = os.environ['APP_SECRET_KEY']
 
 @app.route("/")
-def query():
+def index():
+    committees = db_session.query(Committee).all()
+    return render_template('speaker.html')
+
+@app.route("/all")
+def all():
+    #takes care of the big query
     committees = db_session.query(Committee).all() # or you could have used User.query.all()
     return render_template('query.html', committees=committees)
 
-@app.route("/success")
-def success():
-    return "Thank you!"
+@app.route("/persom")
+def person():
+    return render_template('person.html')
+
+@app.route("/party")
+def party():
+    return render_template('party.html')
+
+@app.route("/district")
+def district():
+    return render_template('district.html')
+
+@app.route("/committee")
+def committee():
+    return render_template('committee.html')
 
 
 if __name__ == '__main__':
--- database.py
+++ database.py
@@ -3,7 +3,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine('sqlite:////opt/services/flaskapp/src/cq_small.sqlite') 
+#engine = create_engine('sqlite:////opt/services/flaskapp/src/cq_small.sqlite')
+engine = create_engine('sqlite:///cq_small.sqlite')
 
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
