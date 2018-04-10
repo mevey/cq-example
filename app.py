228a95a3b013ebb2260b500feaeb00f5b85b5d8e
Stephanie Claudino Daffara <stephaniecd@berkeley.edu>
2018-04-09 19:58:57 -0700

    implemented all queries tab

--- app.py
+++ app.py
@@ -2,18 +2,17 @@ import datetime
 import os
 
 from flask import Flask, render_template, redirect, url_for, request, jsonify
-from models import Committee, Hearing, Speech, Speaker
+from models import Committee, Hearing, Speech, Speaker, Congressmember, Constituency
 from database import db_session
 
 app = Flask(__name__)
 # app.secret_key = os.environ['APP_SECRET_KEY']
 
-
 class Query():
     def __init__(self):
         self.committees = None
+        self.parties = None
         self.data = []
-
 
 class Records():
     def __init__(self):
@@ -25,88 +24,61 @@ class Records():
         self.no_of_records = len(data)
 
 
+def get_committee_query(committee_id, surname, party, district):
+
+    data = db_session.query(Hearing, Speech, Speaker, Congressmember, Constituency)
+
+    if committee_id:
+        committee_id = int(committee_id)
+        data = data.filter(Hearing.committee_id == committee_id)
+
+    if surname:
+        surname = surname.lower()
+        data = data.filter(Speaker.surname == surname)
+
+    if party:
+        data = data.filter(Congressmember.party == party)
+
+    if district:
+        data = data.filter(Constituency.district == district)
+
+    data = data.filter(Hearing.hearing_id == Speech.hearing_id)
+    data = data.filter(Speech.speech_id == Speaker.speech_id)
+    data = data.filter(Speaker.person_id == Congressmember.person_id)
+    data = data.filter(Congressmember.constituency_id == Constituency.constituency_id)
+
+
+    data = data.with_entities(Hearing.hearing_title, Hearing.date, Speaker.surname, Speech.text).distinct().all()
+
+    return data
+
+
 @app.route("/", methods=['GET', 'POST'])
 def index():
     query = Query()
     records = Records()
 
+    # TODO: currently quering only options that HAVE hearings in small database. can query all once we have full db.
     committees = db_session.query(Committee, Hearing)
     committees = committees.filter(Hearing.committee_id == Committee.committee_id)
     committees = committees.with_entities(Committee.committee_id, Committee.committee_name).distinct().all()
     query.committees = committees
 
+    parties = db_session.query(Hearing, Speech, Speaker, Congressmember)
+    parties = parties.filter(Congressmember.person_id == Speaker.person_id)
+    parties = parties.filter(Speaker.speech_id == Speech.speech_id)
+    parties = parties.filter(Speech.hearing_id == Hearing.hearing_id)
+    parties = parties.with_entities(Congressmember.party).distinct().all()
+    query.parties = parties
+
     if request.method == 'POST':
-        committee_id = request.form.get('committee_id')
-        records.data = get_committee_query(committee_id)
+        committee_id = request.form.get('committee')
+        surname = request.form.get('surname')
+        party = request.form.get('party')
+        district = request.form.get('district')
+        records.update_data(get_committee_query(committee_id, surname, party, district))
 
     return render_template('index.html', query=query, records=records)
-
-
-def get_committee_query(committee_id):
-    if committee_id:
-        committee_id = int(committee_id)
-        committees = db_session.query(Hearing, Speech, Speaker)
-        committees = committees.filter(Hearing.committee_id == committee_id)
-        committees = committees.filter(Speech.hearing_id == Hearing.hearing_id)
-        committees = committees.filter(Speaker.speech_id == Speech.speech_id)
-        committees = committees.with_entities(Hearing.hearing_title, Hearing.date, Speech.text, Speaker.surname,
-                                              Hearing.committee_id).all()
-    else:
-        committees = db_session.query(Hearing, Speech, Speaker)
-        committees = committees.filter(Speech.hearing_id == Hearing.hearing_id)
-        committees = committees.filter(Speaker.speech_id == Speech.speech_id)
-        committees = committees.with_entities(Hearing.hearing_title, Hearing.date, Speech.text, Speaker.surname,
-                                              Hearing.committee_id).all()
-
-    return committees
-
-
-@app.route("/person")
-def person():
-    return render_template('person.html')
-
-
-@app.route("/party")
-def party():
-    return render_template('party.html')
-
-
-@app.route("/district")
-def district():
-    return render_template('district.html')
-
-
-# DEPRECATED after I implemented the Committee option in All queries
-@app.route('/committee', methods=['GET', 'POST'])
-def committee():
-    if request.method == 'POST':
-        committee_id = request.form.get('committee_id')
-        if committee_id:
-            committee_id = int(committee_id)
-            data = (db_session.query(Hearing, Speech, Speaker
-                                     ).filter(Hearing.committee_id == committee_id
-                                              ).filter(Speech.hearing_id == Hearing.hearing_id
-                                                       ).filter(Speaker.speech_id == Speech.speech_id
-                                                                ).with_entities(Hearing.hearing_title, Hearing.date,
-                                                                                Speech.text, Speaker.surname,
-                                                                                Hearing.committee_id)).all()
-        else:
-            data = (db_session.query(Hearing, Speech, Speaker
-                                     ).filter(Speech.hearing_id == Hearing.hearing_id
-                                              ).filter(Speaker.speech_id == Speech.speech_id
-                                                       ).with_entities(Hearing.hearing_title, Hearing.date, Speech.text,
-                                                                       Speaker.surname, Hearing.committee_id)).all()
-
-    else:
-        data = {};
-
-    no_of_records = len(data)
-    committees = db_session.query(Committee, Hearing
-                                  ).filter(Hearing.committee_id == Committee.committee_id
-                                           ).with_entities(Committee.committee_id,
-                                                           Committee.committee_name).distinct().all()
-    return render_template('committee.html', committees=committees, data=data, no_of_records=no_of_records)
-
 
 @app.route("/about")
 def about():
