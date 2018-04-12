36a30aac0d602c351ea4146d52c72e4c34b4b75b
mevey <muthoni90@gmail.com>
2018-04-12 12:36:08 -0700

    filters to the side, 10 records max

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import datetime
 import os
 
 from flask import Flask, render_template, redirect, url_for, request, jsonify
-from models import Committee, Hearing, Speech, Speaker, Congressmember, Constituency
+from models import Committee, Hearing, Speech, Speaker, Congressmember, Constituency, Person, ConstituencyCharacteristics
 from database import db_session
 
 app = Flask(__name__)
@@ -21,34 +21,61 @@ class Records():
 
     def update_data(self, data):
         self.data = data
-        self.no_of_records = len(data)
+        self.no_of_records = 0#len(data)
 
 
-def get_committee_query(committee_id, surname, party, district):
+def get_committee_query(committee_name, surname, party, district, state, year, quintile):
 
-    data = db_session.query(Hearing, Speech, Speaker, Congressmember, Constituency)
+    data = db_session.query(Hearing, Speech, Speaker, Person, Committee,  Congressmember, Constituency, ConstituencyCharacteristics)
 
-    if committee_id:
-        committee_id = int(committee_id)
-        data = data.filter(Hearing.committee_id == committee_id)
+    if committee_name:
+        data = data.filter(Committee.committee_name == committee_name)
 
     if surname:
         surname = surname.lower()
-        data = data.filter(Speaker.surname == surname)
+        data = data.filter(Speaker.surname == surname) #Note.message.like("%somestr%")
 
     if party:
         data = data.filter(Congressmember.party == party)
 
+    if year:
+        data = data.filter(Hearing.date == year)
+
     if district:
         data = data.filter(Constituency.district == district)
+
+    if state:
+        data = data.filter(Constituency.state_name == state)
+
+    if quintile:
+        data = data.filter(ConstituencyCharacteristics.density_quintile == quintile)
 
     data = data.filter(Hearing.hearing_id == Speech.hearing_id)
     data = data.filter(Speech.speech_id == Speaker.speech_id)
     data = data.filter(Speaker.person_id == Congressmember.person_id)
-    data = data.filter(Congressmember.constituency_id == Constituency.constituency_id)
+    data = data.filter(Committee.committee_id == Hearing.committee_id)
+    data = data.filter(Person.person_id == Speaker.person_id)
+    data = data.filter(Congressmember.person_id == Person.person_id)
+    data = data.filter(Constituency.constituency_id == Congressmember.constituency_id)
+    data = data.filter(ConstituencyCharacteristics.constituency_id == Constituency.constituency_id)
 
 
-    data = data.with_entities(Hearing.hearing_title, Hearing.date, Speaker.surname, Speech.text).distinct().all()
+    data = data.with_entities(
+        Hearing.hearing_title,
+        Hearing.date,
+        Hearing.url,
+        Speaker.surname,
+        Speech.text,
+        Person.full_name,
+        Person.honorific,
+        Committee.committee_name,
+        Committee.type,
+        Congressmember.party,
+        Congressmember.chamber,
+        Constituency.district,
+        Constituency.state_name,
+        ConstituencyCharacteristics.density_quintile
+    ).limit(10)
 
     return data
 
@@ -58,25 +85,14 @@ def index():
     query = Query()
     records = Records()
 
-    # TODO: currently quering only options that HAVE hearings in small database. can query all once we have full db.
-    committees = db_session.query(Committee, Hearing)
-    committees = committees.filter(Hearing.committee_id == Committee.committee_id)
-    committees = committees.with_entities(Committee.committee_id, Committee.committee_name).distinct().all()
-    query.committees = committees
-
-    parties = db_session.query(Hearing, Speech, Speaker, Congressmember)
-    parties = parties.filter(Congressmember.person_id == Speaker.person_id)
-    parties = parties.filter(Speaker.speech_id == Speech.speech_id)
-    parties = parties.filter(Speech.hearing_id == Hearing.hearing_id)
-    parties = parties.with_entities(Congressmember.party).distinct().all()
-    query.parties = parties
-
-    if request.method == 'POST':
-        committee_id = request.form.get('committee')
-        surname = request.form.get('surname')
-        party = request.form.get('party')
-        district = request.form.get('district')
-        records.update_data(get_committee_query(committee_id, surname, party, district))
+    committee_name = request.form.get('committee')
+    surname = request.form.get('surname')
+    party = request.form.get('party')
+    district = request.form.get('district')
+    state = request.form.get('state')
+    year = request.form.get('year')
+    quintile = request.form.get('quintile')
+    records.update_data(get_committee_query(committee_name, surname, party, district, state, year, quintile))
 
     return render_template('index.html', query=query, records=records)
 
